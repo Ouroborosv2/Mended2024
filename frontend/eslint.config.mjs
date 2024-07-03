18e2ed49a1f1becd84cfa4d00a11b50308bb1c2e
Ubuntu <ubuntu@ip-172-31-10-78.ca-central-1.compute.internal>
2024-07-03 13:09:07 +0000

    Update ESLint configuration with combined settings

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,7 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import reactPlugin from "eslint-plugin-react";
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import nextPlugin from '@next/eslint-plugin-next';
 import { fixupConfigRules } from "@eslint/compat";
@@ -10,22 +10,31 @@ export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     languageOptions: {
-      globals: globals.browser,
+      globals: {
+        ...globals.browser
+      },
       parserOptions: {
         ecmaFeatures: { jsx: true }
       }
     },
     plugins: {
+      'react': reactPlugin,
       'react-hooks': reactHooksPlugin,
       '@next/next': nextPlugin,
     },
     rules: {
+      ...reactPlugin.configs.recommended.rules,
       ...reactHooksPlugin.configs.recommended.rules,
       ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
       'react/react-in-jsx-scope': 'off',
     },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
+  ...fixupConfigRules(pluginJs.configs.recommended),
+  ...fixupConfigRules(tseslint.configs.recommended),
 ];
